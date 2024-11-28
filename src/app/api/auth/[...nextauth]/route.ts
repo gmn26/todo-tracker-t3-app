a2af295af6b3dcd0dfdd16389f8381885f7bd4d2
gmn26 <ghazynovrial@gmail.com>
2024-11-28 22:54:14 +0700

    update : add NextAuth login feature via Discord Provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,12 @@ export const authOptions = {
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID,
             clientSecret: process.env.DISCORD_CLIENT_SECRET,
+            authorization: {
+                url: 'https://discord.com/oauth2/authorize?client_id=1311697701498392668&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&scope=identify+connections+email+guilds+rpc',
+            }
         })
-    ]
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
 }
 
 export const { GET, POST } = handlers;
