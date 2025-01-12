451737b281213f2473c3566316fc0153f7b95449
gmn26 <ghazynovrial@gmail.com>
2025-01-12 23:59:24 +0700

    update : more effective code structure

--- src/app/_components/_server/task/TaskListServer.tsx
+++ src/app/_components/_server/task/TaskListServer.tsx
@@ -1,8 +1,35 @@
 import { api } from "~/trpc/server";
-import TaskListClient from "../../_client/task/TaskListClient";
+import { Calendar, Clock } from "lucide-react";
+import { TimeStampToDate } from "~/utils/ConvertToDate";
+import { TimeStampToHour } from "~/utils/ConvertToHour";
+import DeleteButton from "../../_client/task/DeleteButton";
 
 export default async function TaskListServer({ status }: { status: string }) {
   const tasks = await api.task.fetchByStatus({ status });
 
-  return <TaskListClient tasks={tasks} />;
+  return (
+    <div className="grid grid-cols-2 gap-4 py-3">
+      {tasks?.map((task, index) => (
+        <div
+          key={index}
+          className="group flex flex-col gap-3 rounded-xl border border-slate-300 p-4 shadow"
+        >
+          <div className="flex justify-between">
+            <span className="font-semibold">{task.title}</span>
+            <DeleteButton idTask={task.id} />
+          </div>
+          <p className="grow text-sm text-slate-400">{task.description}</p>
+          <div className="flex justify-between border-t-2 pt-3 *:flex *:items-center *:gap-1 *:text-xs">
+            <span>
+              <Clock size={14} /> {TimeStampToHour(task.createdAt)}
+            </span>
+            <span>
+              <Calendar size={14} />
+              {TimeStampToDate(task.dueDate)}
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/app/_components/_server/task/add/TaskListServer.tsx
+++ src/app/_components/_server/task/add/TaskListServer.tsx
@@ -1,8 +1,40 @@
+import { Calendar, Clock, Dot } from "lucide-react";
 import { api } from "~/trpc/server";
-import TaskListClient from "../../../_client/task/add/TaskListClient";
+import { TimeStampToHour } from "~/utils/ConvertToHour";
 
 export default async function TaskListServer() {
   const tasks = await api.task.fetchTask();
 
-  return <TaskListClient tasks={tasks} />;
+  return (
+    <ul className="flex flex-col gap-4 text-xs">
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <div className="flex flex-col items-center rounded bg-slate-50 p-1">
+            <span className="text-lg font-semibold">{task.title}</span>
+            <div className="flex w-full justify-between">
+              <span className="flex items-center gap-1.5">
+                <Clock size={16} />
+                <span>{TimeStampToHour(task.createdAt)}</span>
+              </span>
+              <span className="flex items-center gap-1.5">
+                <Calendar size={16} />
+                <span>29 November 2024</span>
+              </span>
+            </div>
+            <span className="flex items-center gap-1.5">
+              <span>Status</span>
+              <Dot
+                size={40}
+                className={
+                  task.status != "On Going"
+                    ? "text-green-500"
+                    : "text-yellow-500"
+                }
+              />
+            </span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
